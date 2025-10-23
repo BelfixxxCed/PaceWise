fdc0a78ebb59d2d0d21f7401dc60774870d0fe0e
Cedric Siosana <cedricpsiosana@gmail.com>
2025-10-24 03:08:23 +0800

    fix: Made new svg file to fix different sizes and then also gave highlight on whatever page is being used

--- pacewise/src/components/dashboard/sidebar.tsx
+++ pacewise/src/components/dashboard/sidebar.tsx
@@ -1,7 +1,34 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
+import { 
+  useEffect,
+  useState
+} from 'react';
+import { usePathname } from 'next/navigation';
+
 
 function Sidebar() {
+
+    const [title, setTitle] = useState(0)
+    const path = usePathname();
+
+
+    const title_decide = () => {
+        if (path == '/dashboard/notes'){
+            setTitle(2);
+        } else if (path == "/dashboard/study_schedule"){
+            setTitle(1);
+        } else if (path == "/dashboard/practice_test"){
+            setTitle(3);
+        }
+
+    }
+
+    useEffect(() => {
+        title_decide();
+    }, [])
+
   return (
     <div className='fixed left-0 top-0 h-screen w-20 hover:w-64 bg-[#71D285] z-50 group flex flex-col justify-between transition-all duration-300 ease-in-out'>
       {/* This is for the upper sections */}
@@ -23,7 +50,7 @@ function Sidebar() {
         </div>
 
         <div className="mt-10 flex flex-col gap-2 w-full px-2">
-          <a href="/dashboard/dashboard" className="rounded-lg transition-all duration-200 py-2 px-3 hover:bg-[#E9F5FE] flex items-center gap-3 font-normal hover:font-bold group/item">
+          <a href="/dashboard/dashboard" className={`rounded-lg transition-all duration-200 py-2 px-3 ${title == 0 ? "bg-[#E9F5FE] font-bold" : "hover:bg-[#E9F5FE] hover:font-bold"}  flex items-center gap-3 font-normal  group/item`}>
             <div className='w-10 h-10 flex items-center justify-center flex-shrink-0'>
               <Image 
                 src="/reusable_ui_images/dashboard.svg" 
@@ -38,7 +65,7 @@ function Sidebar() {
             </p>
           </a>
 
-          <a href="/dashboard/study_schedule" className="rounded-lg transition-all duration-200 py-2 px-3 hover:bg-[#E9F5FE] flex items-center gap-3 font-normal hover:font-bold group/item">
+          <a href="/dashboard/study_schedule" className={`${title == 1 ? "bg-[#E9F5FE] font-bold" : "hover:bg-[#E9F5FE] hover:font-bold"} rounded-lg transition-all duration-200 py-2 px-3 hover:bg-[#E9F5FE] flex items-center gap-3 font-normal hover:font-bold group/item`}>
             <div className='w-10 h-10 flex items-center justify-center flex-shrink-0'>
               <Image 
                 src="/reusable_ui_images/study_schedule.svg" 
@@ -53,13 +80,13 @@ function Sidebar() {
             </p>
           </a>
 
-          <a href="/dashboard/notes" className="rounded-lg transition-all duration-200 py-2 px-3 hover:bg-[#E9F5FE] flex items-center gap-3 font-normal hover:font-bold group/item">
+          <a href="/dashboard/notes" className={`${title == 2 ? "bg-[#E9F5FE] font-bold" : "hover:bg-[#E9F5FE] hover:font-bold"} rounded-lg transition-all duration-200 py-2 px-3 hover:bg-[#E9F5FE] flex items-center gap-3 font-normal hover:font-bold group/item`}>
             <div className='w-10 h-10 flex items-center justify-center flex-shrink-0'>
               <Image 
                 src="/reusable_ui_images/notes.svg" 
                 alt="notes button" 
-                width={40} 
-                height={40}
+                width={23} 
+                height={23}
                 className='object-contain'
               />
             </div>
@@ -68,13 +95,13 @@ function Sidebar() {
             </p>
           </a>
 
-          <a href="/dashboard/practice_test" className="rounded-lg transition-all duration-200 py-2 px-3 hover:bg-[#E9F5FE] flex items-center gap-3 font-normal hover:font-bold group/item">
+          <a href="/dashboard/practice_test" className={`${title == 3 ? "bg-[#E9F5FE] font-bold" : "hover:bg-[#E9F5FE] hover:font-bold"} rounded-lg transition-all duration-200 py-2 px-3 hover:bg-[#E9F5FE] flex items-center gap-3 font-normal hover:font-bold group/item`}>
             <div className='w-10 h-10 flex items-center justify-center flex-shrink-0'>
               <Image 
                 src="/reusable_ui_images/practice_test.svg" 
                 alt="practice test button" 
-                width={40} 
-                height={40}
+                width={29} 
+                height={29}
                 className='object-contain'
               />
             </div>
