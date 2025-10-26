9c48fbf04e908253059e391823907be520e713dc
Dave Christian Royo <daveroyo200316@gmail.com>
2025-10-27 00:05:22 +0800

    Save current work before branching

--- pacewise/src/supabase/supabase_client.js
+++ pacewise/src/supabase/supabase_client.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseUrl = "https://flnpmhyvetzssnpsylit.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsbnBtaHl2ZXR6c3NucHN5bGl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1NTE1NDQsImV4cCI6MjA3NjEyNzU0NH0.7MO3cyUVTxX0tPWGtuLvhimnsa5zSTAs3kXri1Gn2s8";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
