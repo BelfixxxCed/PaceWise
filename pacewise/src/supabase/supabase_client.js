2d58d0b661c05d089e00dd9388741f9ae561c5ae
Dave Christian Royo <daveroyo200316@gmail.com>
2025-11-03 14:28:07 +0800

    Modified supabase_client

--- pacewise/src/supabase/supabase_client.js
+++ pacewise/src/supabase/supabase_client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://flnpmhyvetzssnpsylit.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsbnBtaHl2ZXR6c3NucHN5bGl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1NTE1NDQsImV4cCI6MjA3NjEyNzU0NH0.7MO3cyUVTxX0tPWGtuLvhimnsa5zSTAs3kXri1Gn2s8";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+export default supabase; 
