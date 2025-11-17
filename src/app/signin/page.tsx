0162e889bfa9ad5580979a95308ea574c53e401b
chlorineee <147648197+chlorineee@users.noreply.github.com>
2025-11-17 19:20:10 +0800

    feat: redirect authenticated user to main (#42)
    * feat: redirect authenticated user to main
    
    * fix: change /landing to /

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,5 @@
+import SignInPage from "@/components/signin";
 import { Metadata } from "next";
-import DesktopLayout from "@/components/signin/desktopLayout";
-import MobileLayout from "@/components/signin/mobileLayout";
 
 export const metadata: Metadata = {
   title: "Sign in",
@@ -11,19 +10,5 @@ export const metadata: Metadata = {
 };
 
 export default function SignIn() {
-  return (
-    <main className="min-h-screen bg-white flex items-center justify-center overflow-x-clip">
-      <div className="w-full max-w-7xl">
-        {/* Desktop / Tablet */}
-        <div className="hidden md:block">
-          <DesktopLayout />
-        </div>
-
-        {/* Mobile */}
-        <div className="md:hidden">
-          <MobileLayout />
-        </div>
-      </div>
-    </main>
-  );
+  return <SignInPage />;
 }
