25c7e9667ea469265c885c62df078bd9dd223770
Cedric Siosana <cedricpsiosana@gmail.com>
2025-12-02 20:29:50 +0800

    feat: Header now displays user profile

--- src/app/main/notes/notes_with_content/page.tsx
+++ src/app/main/notes/notes_with_content/page.tsx
@@ -12,10 +12,6 @@ export default async function Page({
 
   return (
     <div className="m-2 border-2 p-5 rounded-4xl my-5 mx-35 border-[#71D285]">
-      <input
-        placeholder="Page Title"
-        className="mb-5 poppins-extrabold text-4xl border-0 text-[#8A8A8A]"
-      />
       <div>
         <Notes_component subjectId={subjectId} />
       </div>
